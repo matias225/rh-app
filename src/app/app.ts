e72ee618bf2dd899e3e647d3429c8fe0c921b709
Matías Romani <33184522+matias225@users.noreply.github.com>
2026-08-16 17:27:44 -0300

    First Commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,8 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('rh-app');
